15eab30d0641ab7eda2bd0591de1453f1b4c5143
phuchau.tea <phuchau.tea@gmail.com>
2023-04-13 16:26:13 +0700

    feat: config sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import Button from '~/components/Button';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Button></Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      <h3>Hello, I'm learning React</h3>
     </div>
   );
 }
